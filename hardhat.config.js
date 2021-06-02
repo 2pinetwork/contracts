4cfceebd013b44e47d1027f08a1e064d27f19369
batudo <82539708+batudo@users.noreply.github.com>
2021-06-02 10:35:52 -0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,7 @@ task('accounts', 'Prints the list of accounts', async () => {
   }
 })
 
-const fs = require('fs');
+const fs = require('fs')
 const accounts = JSON.parse(fs.readFileSync(".accounts"));
 
 module.exports = {
