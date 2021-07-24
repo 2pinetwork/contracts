1a1f01b7c3f4f672535c2a62a2dad6d52ccc4b08
Batudo <batudo@2pi.finance>
2021-07-24 15:00:13 -0300

    No more Safe Math on Controller

--- contracts/Controller.sol
+++ contracts/Controller.sol
@@ -5,14 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "../interfaces/IStrategy.sol";
 
 contract Controller is Ownable {
     using SafeERC20 for IERC20;
     using Address for address;
-    using SafeMath for uint256;
 
     address public strategist;
 
