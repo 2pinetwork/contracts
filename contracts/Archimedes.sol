9fda57baaf2d5fc31dd03b1e7e49e5a6a37793ee
Gwydce <gwydce@2pi.finance>
2021-08-28 17:50:22 -0300

    Fixing supertoken dep

--- contracts/Archimedes.sol
+++ contracts/Archimedes.sol
@@ -7,7 +7,8 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
-import { IPiToken } from "../interfaces/IPiToken.sol";
+
+import "../interfaces/IPiToken.sol";
 
 interface IReferral {
     function recordReferral(address, address referrer) external;
--- interfaces/IPiToken.sol
+++ interfaces/IPiToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import { ISuperToken } from "../vendor_contracts/NativeSuperTokenProxy.sol";
+import "../vendor_contracts/NativeSuperTokenProxy.sol";
 
 // Used in Archimedes
 interface IPiToken is ISuperToken {
