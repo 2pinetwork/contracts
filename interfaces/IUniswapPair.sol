20a21a899693ae38f2f7362366a1e31610a87386
Néstor Coppi <nestorcoppi@gmail.com>
2021-11-08 21:50:13 -0300

    UniZap (swap, join, split LPs) (#14)
    Co-authored-by: Gwydce <gwydce@2pi.finance>

---
+++ interfaces/IUniswapPair.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+interface IUniswapPair {
+    function approve(address, uint) external returns (bool);
+    function balanceOf(address) external view returns (uint);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function skim(address to) external;
+}
--- interfaces/IUniswapRouter.sol
+++ interfaces/IUniswapRouter.sol
@@ -3,11 +3,39 @@
 pragma solidity ^0.8.9;
 
 interface IUniswapRouter {
-    function swapExactTokensForTokens(
-        uint amountIn,
-        uint amountOutMin,
-        address[] calldata path,
-        address to,
-        uint deadline
-    ) external returns (uint[] memory amounts);
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB, uint liquidity);
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+  function removeLiquidityETH(
+      address token,
+      uint liquidity,
+      uint amountTokenMin,
+      uint amountETHMin,
+      address to,
+      uint deadline
+  ) external returns (uint amountToken, uint amountETH);
 }
