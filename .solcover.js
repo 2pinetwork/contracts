ef89e9ed3c202b1a8321a15fca30e6c2b9a795b2
Gwydce <gwydce@2pi.finance>
2022-01-14 18:09:46 -0300

    Parallel test + coverage

--- .solcover.js
+++ .solcover.js
@@ -1,15 +1,24 @@
 module.exports = {
   enableTimeouts: false,
   skipFiles: [
+    'mocks/ArchimedesAPIMock.sol',
+    'mocks/ArchimedesMock.sol',
+    'mocks/BridgedPiTokenMock.sol',
     'mocks/CurvePoolMock.sol',
     'mocks/CurveRewardsGaugeMock.sol',
     'mocks/DataProviderMock.sol',
+    'mocks/DistributorMock.sol',
     'mocks/FarmMock.sol',
     'mocks/IncentivesControllerMock.sol',
+    'mocks/PiTokenMock.sol',
     'mocks/PoolMock.sol',
     'mocks/PriceFeedMock.sol',
+    'mocks/TestNetMint.sol',
     'mocks/TokenMock.sol',
     'mocks/UniswapRouterMock.sol',
     'mocks/WETHMock.sol',
+    'ControllerLPWithoutStrat.sol',
+    'PiOracle.sol',
+    'UniZap.sol'
   ]
 }
--- scripts/run_coverage_and_copy_file.sh
+++ scripts/run_coverage_and_copy_file.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-npx hardhat coverage --testfiles $1 > /dev/null
+if [[ "$1" =~ integration ]]; then
+  # HARDHAT INTEGRATION should be set
+  echo "Running preprocess"
+  npx hardhat preprocess
+fi
+npx hardhat coverage --testfiles $1
 
 array=$(echo $1 | sed "s/,/\n/g")
 file=(${array[0]})
@@ -9,3 +14,5 @@ file=$(echo $file | sed "s/\//_/g") # change / for _
 file=$(echo $file | sed "s/-test\.js/.json/g") # change / for _
 
 cp /app/coverage.json /coverages/$file
+chmod 666 /coverages/$file
+chown 1000:1000 /coverages/$file
