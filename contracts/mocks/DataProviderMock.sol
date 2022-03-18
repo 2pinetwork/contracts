e270ea87578201e5d137a0ad5c0c1e0a668b2fd1
Batudo <82539708+batudo@users.noreply.github.com>
2022-03-18 13:27:20 -0300

    Solidity 0.8.13 (#37)

--- contracts/mocks/DataProviderMock.sol
+++ contracts/mocks/DataProviderMock.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.11;
+pragma solidity 0.8.13;
 
 contract DataProviderMock {
     // For testing use the counters/balance will be based
--- interfaces/ICurvePool.sol
+++ interfaces/ICurvePool.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.11;
+pragma solidity 0.8.13;
 
 interface ICurvePool {
     // _use_underlying If True, withdraw underlying assets instead of aTokens
