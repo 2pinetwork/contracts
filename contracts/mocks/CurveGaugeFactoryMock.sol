c9377cf0e7df7a90762e26a5d58755e8d071d475
Néstor Coppi <nestorcoppi@gmail.com>
2022-05-29 21:23:53 -0300

    Fix Curve tests

---
+++ contracts/mocks/CurveGaugeFactoryMock.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import "../../interfaces/ICurve.sol";
+
+interface ICurveMock is ICurveGauge {
+    function claimed(address) external;
+}
+
+contract CurveGaugeFactoryMock {
+    using SafeERC20 for IERC20;
+
+    IERC20 CRV = IERC20(0xED8CAB8a931A4C0489ad3E3FB5BdEA84f74fD23E);
+
+    function mint(address _gauge) public {
+        uint _Cbalance = CRV.balanceOf(address(this));
+        uint _claimable = ICurveGauge(_gauge).claimable_tokens(msg.sender);
+
+        if (_Cbalance > 0 && _claimable > 0) {
+            CRV.safeTransfer(msg.sender, _claimable);
+            ICurveMock(_gauge).claimed(msg.sender);
+        }
+    }
+}
--- contracts/mocks/CurveRewardsGaugeMock.sol
+++ contracts/mocks/CurveRewardsGaugeMock.sol
@@ -12,8 +12,15 @@ contract CurveRewardsGaugeMock {
     IERC20 WMATIC = IERC20(0x73511669fd4dE447feD18BB79bAFeAC93aB7F31f);
     IERC20 CRV = IERC20(0xED8CAB8a931A4C0489ad3E3FB5BdEA84f74fD23E);
 
+    mapping(address => mapping(address => uint)) private claimable;
     mapping(address => uint) private counter;
     address[] private holders;
+    address[] private claimers;
+
+    function setClaimable(address _token, address _wallet, uint _amount) external {
+        claimable[_token][_wallet] += _amount;
+        claimers.push(_wallet);
+    }
 
     function reset() public {
         WMATIC.transfer(address(1), WMATIC.balanceOf(address(this)));
@@ -22,17 +29,41 @@ contract CurveRewardsGaugeMock {
         for (uint i; i < holders.length; i++) {
             counter[holders[i]] = 0;
         }
+
+        for (uint i; i < claimers.length; i++) {
+            claimable[address(CRV)][claimers[i]] = 0;
+            claimable[address(WMATIC)][claimers[i]] = 0;
+        }
     }
 
-    function balanceOf(address account) external view returns (uint) {
+    function balanceOf(address account) public view returns (uint) {
         return counter[account];
     }
-    function claim_rewards(address _addr) external {
-        uint _Wbalance = WMATIC.balanceOf(address(this));
-        uint _Cbalance = CRV.balanceOf(address(this));
+    function claimable_tokens(address wallet) public view returns (uint) {
+        return claimable[address(CRV)][wallet];
+    }
+    function claimable_reward(address _wallet, address _token) external view returns (uint) {
+        return claimable[_token][_wallet];
+    }
 
-        if (_Wbalance > 0) { WMATIC.safeTransfer(_addr, _Wbalance); }
-        if (_Cbalance > 0) { CRV.safeTransfer(_addr, _Cbalance); }
+    function reward_count() public pure returns (uint) {
+        return 1;
+    }
+
+    function reward_tokens(uint) public view returns (address) {
+        return address(WMATIC);
+    }
+
+    function claim_rewards() external {
+        uint _claimable = claimable[address(WMATIC)][msg.sender];
+
+        if (WMATIC.balanceOf(address(this)) > 0 && _claimable > 0) {
+            WMATIC.safeTransfer(msg.sender, _claimable);
+            claimable[address(WMATIC)][msg.sender] = 0;
+        }
+    }
+    function claimed(address _wallet) external {
+        claimable[address(CRV)][_wallet] = 0;
     }
 
     function deposit(uint _value) external {
