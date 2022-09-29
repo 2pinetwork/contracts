2ef817ebf3024ce58e94a53323f1b8ac994f8936
Batudo <batudo@2pi.network>
2022-09-29 09:33:03 -0300

    Set useNative on strategy when configured

--- scripts/5-deploy_strategies.js
+++ scripts/5-deploy_strategies.js
@@ -86,6 +86,10 @@ async function main() {
     await (await strategy.setMaxPriceOffset(24 * 3600, callArgs())).wait()
     await (await strategy.setPriceFeed(pool.token, deploy.chainlink[pool.token], callArgs())).wait()
 
+    if (pool.useNative) {
+      await (await strategy.setUseNative(true)).wait()
+    }
+
     for (let token of pool.rewards) {
       console.log(`Configuring reward: ${token}`)
       await (await strategy.setPriceFeed(token, deploy.chainlink[token], callArgs())).wait()
