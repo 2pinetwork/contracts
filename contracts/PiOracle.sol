7bee98c40fa4873363044f62034d087f667eb592
Batudo <batudo@2pi.network>
2022-11-14 12:46:23 -0300

    PiOracle with multiples LPs (WIP)

--- contracts/PiOracle.sol
+++ contracts/PiOracle.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.6.6;
 
-import '@uniswap/lib/contracts/libraries/FixedPoint.sol';
-// import 'hardhat/console.sol';
+import "@uniswap/lib/contracts/libraries/FixedPoint.sol";
+import "hardhat/console.sol";
 
 // Manual interface declaration because of the @openzeppelin lib is for solidity 0.8
 interface IERC20MetadataAlt {
@@ -28,22 +28,23 @@ library UniswapV2OracleLibrary {
 
     // produces the cumulative price using counterfactuals to save gas and avoid a call to sync.
     function currentCumulativePrices(
-        address pair
-    ) internal view returns (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) {
-        blockTimestamp = _currentBlockTimestamp();
-        price0Cumulative = IUniswapV2Pair(pair).price0CumulativeLast();
-        price1Cumulative = IUniswapV2Pair(pair).price1CumulativeLast();
+        address _pair
+    ) internal view returns (uint _price0Cumulative, uint _price1Cumulative, uint32 _blockTimestamp) {
+        _blockTimestamp = _currentBlockTimestamp();
+        _price0Cumulative = IUniswapV2Pair(_pair).price0CumulativeLast();
+        _price1Cumulative = IUniswapV2Pair(_pair).price1CumulativeLast();
 
         // if time has elapsed since the last update on the pair, mock the accumulated price values
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = IUniswapV2Pair(pair).getReserves();
-        if (blockTimestampLast != blockTimestamp) {
+        (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = IUniswapV2Pair(_pair).getReserves();
+
+        if (_blockTimestampLast != _blockTimestamp) {
             // subtraction overflow is desired
-            uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+            uint32 _timeElapsed = _blockTimestamp - _blockTimestampLast;
             // addition overflow is desired
             // counterfactual
-            price0Cumulative += uint(FixedPoint.fraction(reserve1, reserve0)._x) * timeElapsed;
+            _price0Cumulative += uint(FixedPoint.fraction(_reserve1, _reserve0)._x) * _timeElapsed;
             // counterfactual
-            price1Cumulative += uint(FixedPoint.fraction(reserve0, reserve1)._x) * timeElapsed;
+            _price1Cumulative += uint(FixedPoint.fraction(_reserve0, _reserve1)._x) * _timeElapsed;
         }
     }
 }
@@ -53,35 +54,61 @@ contract PiOracle {
 
     uint public constant PERIOD = 1 minutes;
 
-    IUniswapV2Pair immutable lp;
+    struct path {
+        IUniswapV2Pair lp;
+        uint priceCumulativeLast;
+        uint32 blockTimestampLast;
+        FixedPoint.uq112x112 priceAverage;
+        address next;
+    }
 
-    address public immutable PiToken;
-    bool    public immutable firstToken;
-    uint    public priceCumulativeLast;
-    uint32  public blockTimestampLast;
-    FixedPoint.uq112x112 public priceAverage;
+    path[] paths;
+
+    address public immutable target;
 
     mapping(address => bool) public admins;
 
-    constructor(IUniswapV2Pair _lp, address _pi) public {
-        PiToken = _pi;
-        lp = _lp;
+    constructor(IUniswapV2Pair[] memory _lps, address _target, address _priceSource) public {
+        require(_lps.length > 0, "No LPs!");
+        require(_target != address(0), "Target zero!");
+        require(_priceSource != address(0), "Source zero!");
+        require(_lps[0].token0() == _target || _lps[0].token1() == _target, "No target");
+        require(_lps[_lps.length - 1].token0() == _priceSource || _lps[_lps.length - 1].token1() == _priceSource, "No source");
 
-        bool _firstToken = _lp.token0() == _pi;
-        firstToken = _firstToken;
+        target = _target;
 
-        require(_firstToken || _lp.token1() == _pi, "No Pi-LP");
+        address _next = _lps[0].token0() == _target ? _lps[0].token1() : _lps[0].token0();
 
-        uint112 reserve0;
-        uint112 reserve1;
-        (reserve0, reserve1, blockTimestampLast) = _lp.getReserves();
+        for (uint i = 0; i < _lps.length; i++) {
+            path memory _path;
+            IUniswapV2Pair _lp = _lps[i];
 
-        require(reserve0 > 0 && reserve1 > 0, 'NO_RESERVES'); // ensure that there's liquidity in the pair
+            _path.lp = _lp;
+            _path.next = _next;
 
-        if (_firstToken) {
-            priceCumulativeLast = _lp.price0CumulativeLast(); // fetch the current accumulated price value (1 / 0)
-        } else {
-            priceCumulativeLast = _lp.price1CumulativeLast(); // fetch the current accumulated price value (0 / 1)
+            (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast) = _lp.getReserves();
+
+            _path.blockTimestampLast = _blockTimestampLast;
+
+            require(_reserve0 > 0 && _reserve1 > 0, "NO_RESERVES");
+
+            console.log("pre _next", _next);
+
+            if (_lp.token1() == _next) {
+                // fetch the current accumulated price value (1 / 0)
+                _path.priceCumulativeLast = _lp.price0CumulativeLast();
+                _next = _lp.token1();
+            } else {
+                // fetch the current accumulated price value (0 / 1)
+                _path.priceCumulativeLast = _lp.price1CumulativeLast();
+                _next = _lp.token0();
+            }
+
+            console.log("_path1", _path.next, _path.lp.token0(), _path.lp.token1());
+            console.log("_path2", _path.priceCumulativeLast);
+            console.log("_next", _next);
+
+            paths.push(_path);
         }
 
         admins[msg.sender] = true;
@@ -97,56 +124,79 @@ contract PiOracle {
     }
 
     function update() external {
-        (uint price0Cumulative, uint price1Cumulative, uint32 blockTimestamp) =
-            UniswapV2OracleLibrary.currentCumulativePrices(address(lp));
-        uint32 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
+        console.log("Update");
 
-        // ensure that at least one full period has passed since the last update
-        require(timeElapsed >= PERIOD, 'PERIOD_NOT_ELAPSED');
+        for (uint i = 0; i < paths.length; i++) {
+            path memory _path = paths[i];
 
-        // overflow is desired, casting never truncates
-        // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
-        if (firstToken) {
-            priceAverage = FixedPoint.uq112x112(uint224((price0Cumulative - priceCumulativeLast) / timeElapsed));
-            priceCumulativeLast = price0Cumulative;
-        } else {
-            priceAverage = FixedPoint.uq112x112(uint224((price1Cumulative - priceCumulativeLast) / timeElapsed));
-            priceCumulativeLast = price1Cumulative;
+            (uint _price0Cumulative, uint _price1Cumulative, uint32 _blockTimestamp) =
+                UniswapV2OracleLibrary.currentCumulativePrices(address(_path.lp));
+            uint32 _timeElapsed = _blockTimestamp - _path.blockTimestampLast; // overflow is desired
+
+            // ensure that at least one full period has passed since the last update
+            console.log("PERIOD DATA", _timeElapsed, _blockTimestamp, _path.blockTimestampLast);
+            require(_timeElapsed >= PERIOD, "PERIOD_NOT_ELAPSED");
+
+            // overflow is desired, casting never truncates
+            // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
+            if (_path.next == _path.lp.token1()) {
+                _path.priceAverage = FixedPoint.uq112x112(uint224((_price0Cumulative - _path.priceCumulativeLast) / _timeElapsed));
+                _path.priceCumulativeLast = _price0Cumulative;
+            } else {
+                _path.priceAverage = FixedPoint.uq112x112(uint224((_price1Cumulative - _path.priceCumulativeLast) / _timeElapsed));
+                _path.priceCumulativeLast = _price1Cumulative;
+            }
+
+            _path.blockTimestampLast = _blockTimestamp;
+
+            paths[i] = _path;
         }
-
-        blockTimestampLast = blockTimestamp;
     }
 
     // Chainlink like method
-    function latestRoundData() external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
-        uint firstTokenPrecision = 10 ** uint(IERC20MetadataAlt(firstToken ? lp.token0() : lp.token1()).decimals());
-        uint secondTokenPrecision = 10 ** uint(IERC20MetadataAlt(firstToken ? lp.token1() : lp.token0()).decimals());
+    function latestRoundData() external view returns (
+        uint80 _roundId,
+        int256 _answer,
+        uint256 _startedAt,
+        uint256 _updatedAt,
+        uint80 _answeredInRound
+    ) {
+        uint[] memory _answers = new uint[](paths.length);
+        path memory _lastPath = paths[paths.length - 1];
 
-        // require(secondTokenPrecision > 0 && secondTokenPrecision <= 1e18, "weird secondary token decimals");
-        // require(secondTokenPrecision > 0 && secondTokenPrecision <= 1e18, "weird secondary token decimals");
+        for (uint i = 0; i < paths.length; i++) {
+            path memory _path = paths[i];
+            IUniswapV2Pair _lp = _path.lp;
+            bool _firstToken = _lp.token1() == _path.next;
+            uint _firstTokenPrecision = 10 ** uint(IERC20MetadataAlt(_firstToken ? _lp.token0() : _lp.token1()).decimals());
+            uint _secondTokenPrecision = 10 ** uint(IERC20MetadataAlt(_firstToken ? _lp.token1() : _lp.token0()).decimals());
 
+            // price decimals
+            // uint pricePrecision = 10 ** uint(decimals());
+            // we concat first token precision with the chainlink price precision
+            uint _pricePrecision = _firstTokenPrecision * (10 ** uint(decimals()));
 
-        // price decimals
-        // uint pricePrecision = 10 ** uint(decimals());
-        // we concat first token precision with the chainlink price precision
-        uint pricePrecision = firstTokenPrecision * (10 ** uint(decimals()));
+            uint _price = uint(_path.priceAverage.mul(_pricePrecision).decode144());
 
-        uint price = uint(priceAverage.mul(pricePrecision).decode144());
+            // in case the price is too low
+            if (_price >= _secondTokenPrecision) {
+                _price /= _secondTokenPrecision;
+            } else {
+                _price = 0;
+            }
 
-        // in case the price is too low
-        if (price >= secondTokenPrecision) {
-            price /= secondTokenPrecision;
-        } else {
-            price = 0;
+            console.log("Price", i, _price);
+            _answers[i] = _price;
         }
 
-        answer = int256(price);
-        require(answer >= 0, "Underflow price");
+        console.log("Answers", _answers[0], _answers[1]);
+        _answer = int256(_answers[1]);
+        require(_answer >= 0, "Underflow price");
 
-        roundId = blockTimestampLast;
-        startedAt = blockTimestampLast;
-        updatedAt = blockTimestampLast;
-        answeredInRound = blockTimestampLast;
+        _roundId = _lastPath.blockTimestampLast;
+        _startedAt = _lastPath.blockTimestampLast;
+        _updatedAt = _lastPath.blockTimestampLast;
+        _answeredInRound = _lastPath.blockTimestampLast;
     }
 
     function decimals() public pure returns (uint8) {
--- test/integration/PiOracle-test.js
+++ test/integration/PiOracle-test.js
@@ -6,9 +6,10 @@ const {
 const {
   createPiTokenExchangePair,
   resetHardhat,
+  setWethBalanceFor
 } = require('./helpers')
 
-describe('PiOracle', () => {
+describe('PiOracle single LP', () => {
   let oracle
   let pair
   let PERIOD
@@ -16,8 +17,10 @@ describe('PiOracle', () => {
   before(async () => {
     await resetHardhat()
 
+    console.log('Addresses, PI, WMATIC', PiToken.address, WMATIC.address);
+
     pair = await createPiTokenExchangePair()
-    oracle = await deploy('PiOracle', pair, PiToken.address)
+    oracle = await deploy('PiOracle', [pair], PiToken.address, WMATIC.address)
     PERIOD = parseInt(await oracle.PERIOD(), 10)
   })
 
@@ -27,7 +30,7 @@ describe('PiOracle', () => {
     let time = (await ethers.provider.getBlock()).timestamp
 
     for (let i = 0; i < 5; i++) {
-      let lastPrice = (await oracle.latestRoundData()).answer
+      let lastPrice = (await oracle.latestRoundData())._answer
 
       // has to wait X minutes
       await network.provider.send('evm_setNextBlockTimestamp', [time += PERIOD + 1])
@@ -41,7 +44,7 @@ describe('PiOracle', () => {
       // First lastPrice is 0
       if (i == 0) continue
 
-      expect((await oracle.latestRoundData()).answer, `Loop: ${i}`).to.be.below(
+      expect((await oracle.latestRoundData())._answer, `Loop: ${i}`).to.be.below(
         lastPrice
       )
     }
@@ -54,7 +57,7 @@ describe('PiOracle', () => {
     let time = (await ethers.provider.getBlock()).timestamp
 
     for (let i = 0; i < 5; i++) {
-      let lastPrice = (await oracle.latestRoundData()).answer
+      let lastPrice = (await oracle.latestRoundData())._answer
 
       // has to wait X minutes
       await network.provider.send('evm_setNextBlockTimestamp', [time += PERIOD + 1])
@@ -65,7 +68,86 @@ describe('PiOracle', () => {
       )
       await waitFor(oracle.update())
 
-      expect((await oracle.latestRoundData()).answer, `Loop: ${i}`).to.be.above(
+      expect((await oracle.latestRoundData())._answer, `Loop: ${i}`).to.be.above(
+        lastPrice
+      )
+    }
+  })
+})
+
+describe.only('PiOracle multi LP', () => {
+  let oracle
+  let owner
+  let wethWmaticPair
+  let wmaticUsdcPair
+  let PERIOD
+
+  before(async () => {
+    await resetHardhat()
+
+    console.log('WETH', WETH.address)
+    console.log('WMATIC', WMATIC.address)
+    console.log('USDC', USDC.address)
+
+    wethWmaticPair = '0xc4e595acDD7d12feC385E5dA5D43160e8A0bAC0E'
+    wmaticUsdcPair = '0xcd353F79d9FADe311fC3119B841e1f456b54e858'
+    owner = await ethers.getSigner()
+    oracle = await deploy('PiOracle', [wethWmaticPair, wmaticUsdcPair], WETH.address, USDC.address)
+    PERIOD = parseInt(await oracle.PERIOD(), 10)
+  })
+
+  it.only('should get less price each swap', async () => {
+    const route = [WETH.address, WMATIC.address, USDC.address]
+
+    await setWethBalanceFor(owner.address, '100')
+    await WETH.approve(exchange.address, ethers.utils.parseUnits('100'))
+
+    let time = (await ethers.provider.getBlock()).timestamp
+
+    for (let i = 0; i < 5; i++) {
+      let lastPrice = (await oracle.latestRoundData())._answer
+
+      console.log('Some loop', i, time, PERIOD)
+
+      // has to wait X minutes
+      await network.provider.send('evm_setNextBlockTimestamp', [time += PERIOD + 100])
+      await waitFor(
+        exchange.swapExactTokensForTokens(
+          10e18 + '', 1, route, owner.address, time + 10
+        )
+      )
+      await waitFor(oracle.update())
+
+      // First lastPrice is 0
+      if (i == 0) continue
+
+      console.log('Answer', i, (await oracle.latestRoundData()).toString())
+
+      expect((await oracle.latestRoundData())._answer, `Loop: ${i}`).to.be.above(
+        lastPrice
+      )
+    }
+  })
+
+  it('should get more price each swap', async () => {
+    await waitFor(WMATIC.deposit({ value: 6e10 + '' }))
+    const route = [WMATIC.address, PiToken.address]
+
+    let time = (await ethers.provider.getBlock()).timestamp
+
+    for (let i = 0; i < 5; i++) {
+      let lastPrice = (await oracle.latestRoundData())._answer
+
+      // has to wait X minutes
+      await network.provider.send('evm_setNextBlockTimestamp', [time += PERIOD + 1])
+      await waitFor(
+        exchange.swapExactTokensForTokens(
+          1e18 + '', 1, route, owner.address, time + 10
+        )
+      )
+      await waitFor(oracle.update())
+
+      expect((await oracle.latestRoundData())._answer, `Loop: ${i}`).to.be.above(
         lastPrice
       )
     }
